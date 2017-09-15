57211a43b34dfbde82dbef737805a7ce818b360e
TheTOXIN <thetoksin@mail.ru>
2017-09-15 21:44:19 +0300

    Add big task - 2048 The game

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/CookedOrderEventDataRow.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+import com.javarush.task.task27.task2712.kitchen.Dish;
+
+import java.util.Date;
+import java.util.List;
+
+public class CookedOrderEventDataRow implements EventDataRow {
+    private String tabletName;
+    private String cookName;
+    private int cookingTimeSecond;
+    private List<Dish> cookingDishs;
+    private Date currentDate;
+
+    public CookedOrderEventDataRow(String tabletName, String cookName, int cookingTimeSeconds, List<Dish> cookingDishs) {
+        this.tabletName = tabletName;
+        this.cookName = cookName;
+        this.cookingTimeSecond = cookingTimeSeconds;
+        this.cookingDishs = cookingDishs;
+        currentDate = new Date();
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/EventDataRow.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+import java.util.Date;
+
+public interface EventDataRow {
+
+}
+
